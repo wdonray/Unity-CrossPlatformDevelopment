8711738b147a5b64d8f2e5ecad042927236086b9
Donray Williams <w.donray@yahoo.com>
2017-04-26 17:50:21 -0500

    Work

---
+++ Assets/Scripts/GameBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Spawn Boxes Behavior
+public class GameBehavior : MonoBehaviour
+{
+
+    public List<GameObject> Boxes;
+    public int Box_Number;
+    public string Say;
+    public GameObject Box_Prefab;
+
+    // Use this for initialization
+    void Start()
+    {
+        Log();
+        Boxes = new List<GameObject>();
+    }
+    private void Update()
+    {
+        GameObject Box;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Log();
+            Box = Instantiate(Box_Prefab) as GameObject;
+            Boxes.Add(Box);
+            Box_Number++;
+            Renderer color = Box.GetComponent<Renderer>();
+            color.material.color = new Color(Random.Range(0, 255), Random.Range(0, 255), Random.Range(0, 255));
+        }
+    }
+    void Log()
+    {
+        Debug.Log(Say + " " + Box_Number);
+    }
+}
---
+++ Assets/Scripts/Log.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Log : MonoBehaviour
+{
+
+    public Text Log_Text;
+    public Text Current_Pos;
+
+    private Vector2 Pos;
+    // Use this for initialization
+    void Start()
+    {
+        Log_Text.text = "Welcome to Bright Souls 3";
+        Pos = new Vector2(0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Input_Check();
+        Current_Pos.text = "Current Pos: " + Pos.ToString();
+        if (Pos == new Vector2(1, 0))
+            Log_Text.text = "You Here";
+
+    }
+
+    void Input_Check()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+            Pos += new Vector2(-1, 0);
+        else if (Input.GetKeyDown(KeyCode.D))
+            Pos += new Vector2(1, 0);
+        else if (Input.GetKeyDown(KeyCode.W))
+            Pos += new Vector2(0, 1);
+        else if (Input.GetKeyDown(KeyCode.S))
+            Pos += new Vector2(0, -1);
+    }
+}
