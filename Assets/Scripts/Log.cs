dc40f7c9c64364f403a95a05b4844298c95caebb
Donray Williams <w.donray@yahoo.com>
2017-04-27 17:22:33 -0500

    Work

--- Assets/Scripts/Log.cs
+++ Assets/Scripts/Log.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Log : MonoBehaviour
 {
@@ -10,32 +11,83 @@ public class Log : MonoBehaviour
     public Text Current_Pos;
 
     private Vector2 Pos;
+    private Vector2 Goal;
+    private Dictionary<Vector2, GameObject> Nodes = new Dictionary<Vector2, GameObject>();
+    private GameObject go;
+
     // Use this for initialization
     void Start()
     {
         Log_Text.text = "Welcome to Bright Souls 3";
         Pos = new Vector2(0, 0);
+        while (Goal == Pos)
+        {
+            Goal = new Vector2((Random.Range(0, 5)), (Random.Range(0, 5)));
+        }
+        Grid();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Input_Check();
-        Current_Pos.text = "Current Pos: " + Pos.ToString();
-        if (Pos == new Vector2(1, 0))
-            Log_Text.text = "You Here";
+        Scene scene = SceneManager.GetActiveScene();
+        Nodes[Pos].GetComponent<Renderer>().material.color = Color.magenta;
+        Nodes[Goal].GetComponent<Renderer>().material.color = Color.yellow;
 
+        Input_Check();
+
+        Current_Pos.text = "Current Pos\n  " + Pos.ToString();
+
+        if (Pos == Goal)
+            Log_Text.text = "You Here";
+        else
+            Log_Text.text = "You are not here";
     }
 
     void Input_Check()
     {
         if (Input.GetKeyDown(KeyCode.A))
-            Pos += new Vector2(-1, 0);
+        {
+            if (Pos.x > 0)
+            {
+                Nodes[Pos].GetComponent<Renderer>().material.color = Color.white;
+                Pos += new Vector2(-1, 0);
+            }
+        }
         else if (Input.GetKeyDown(KeyCode.D))
-            Pos += new Vector2(1, 0);
+        {
+            if (Pos.x < 4)
+            {
+                Nodes[Pos].GetComponent<Renderer>().material.color = Color.white;
+                Pos += new Vector2(1, 0);
+            }
+        }
         else if (Input.GetKeyDown(KeyCode.W))
-            Pos += new Vector2(0, 1);
+        {
+            if (Pos.y < 4)
+            {
+                Nodes[Pos].GetComponent<Renderer>().material.color = Color.white;
+                Pos += new Vector2(0, 1);
+            }
+        }
         else if (Input.GetKeyDown(KeyCode.S))
-            Pos += new Vector2(0, -1);
+            if (Pos.y > 0)
+            {
+                Nodes[Pos].GetComponent<Renderer>().material.color = Color.white;
+                Pos += new Vector2(0, -1);
+            }
+    }
+    void Grid()
+    {
+        int count = 0;
+        for (var i = 0; i < 5; i++)
+            for (var j = 0; j < 5; j++)
+            {
+                count += 1;
+                Vector2 v = new Vector2(i, j);
+                go = GameObject.CreatePrimitive(PrimitiveType.Quad);
+                go.transform.position = new Vector3(v.x, v.y, go.transform.position.z);
+                Nodes.Add(v, go);
+            }
     }
 }
