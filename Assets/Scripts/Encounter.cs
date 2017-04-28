3a225f4f1babf283aee932e60625b4df62c9dd66
Donray Williams <w.donray@yahoo.com>
2017-04-28 18:01:30 -0500

    Lots of changes to 0.main

---
+++ Assets/Scripts/Encounter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Encounter : MonoBehaviour
+{
+    //public UnityEngine.UI.Text Win_Text; 
+
+    private Renderer rend;
+    [SerializeField]
+    private GameBehavior Goal_Test;
+    private void Start()
+    {
+        Goal_Test = FindObjectOfType<GameBehavior>();
+        rend = GetComponentInParent<Renderer>();
+        //Goal_Test.GetComponent<Renderer>().material.color = Color.yellow;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            rend.material.color = Random.ColorHSV();
+            if (this.gameObject == Goal_Test.Goal)
+            {
+                Debug.Log("You win");
+            }
+        }
+    }
+}
--- Assets/Scripts/GameBehavior.cs
+++ Assets/Scripts/GameBehavior.cs
@@ -7,31 +7,75 @@ public class GameBehavior : MonoBehaviour
 {
 
     public List<GameObject> Boxes;
-    public int Box_Number;
     public string Say;
     public GameObject Box_Prefab;
-
+    public int dims;
+    public int Box_Count;
+    public GameObject Player;
+    
+    public GameObject Goal;
     // Use this for initialization
     void Start()
     {
-        Log();
         Boxes = new List<GameObject>();
+        Create();
+        Goal = Boxes[Random.Range(0, Boxes.Count)].transform.GetChild(0).gameObject;
+        Goal.GetComponentInParent<Renderer>().material.color = Color.yellow;
+        Box_Count = 0;
+        Log();
     }
     private void Update()
     {
-        GameObject Box;
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Log();
-            Box = Instantiate(Box_Prefab) as GameObject;
-            Boxes.Add(Box);
-            Box_Number++;
-            Renderer color = Box.GetComponent<Renderer>();
-            color.material.color = new Color(Random.Range(0, 255), Random.Range(0, 255), Random.Range(0, 255));
-        }
+        if (Input.GetKeyDown(KeyCode.Delete))
+            Delete();
     }
     void Log()
     {
-        Debug.Log(Say + " " + Box_Number);
+        Debug.Log(Say + " " + Box_Count);
+    }
+    void Create()
+    {
+        GameObject Box;
+
+        for (int i = 0; i < dims; i++)
+        {
+            for (int j = 0; j < dims; j++)
+            {
+                Box = Instantiate(Box_Prefab) as GameObject;
+                GameObject GameObj_Trigger = new GameObject();
+                GameObj_Trigger.transform.parent = Box.transform;
+                Box.transform.position = Box.transform.position;
+                BoxCollider GameObj_Collider = GameObj_Trigger.AddComponent<BoxCollider>();
+                GameObj_Collider.center = new Vector3(0, 1.5f, 0);
+                GameObj_Collider.size = new Vector3(1, 1, 1);
+                GameObj_Collider.isTrigger = true;
+                GameObj_Collider.name = "Trigger";
+                GameObj_Trigger.AddComponent<Encounter>();
+                Boxes.Add(Box);
+                Box.transform.position = new Vector3(i * 1.1f, 0, j * 1.1f);
+            }
+        }
+        Player.GetComponent<Rigidbody>().useGravity = true;
+        Box_Count = Boxes.Count;
+        //Goal = Boxes[Random.Range(0, Boxes.Count)].transform.GetChild(0).gameObject;
+        Goal = Boxes[0].transform.GetChild(0).gameObject;
+        Log();
+    }
+    void Delete()
+    {
+        foreach (GameObject g in Boxes)
+        {
+            DestroyImmediate(g);
+        }
+       // Player.transform.position = new Vector3(5, 5, 5);
     }
 }
+//if (Input.GetKeyDown(KeyCode.Space))
+//{
+//    //Log();
+//    //Box = Instantiate(Box_Prefab) as GameObject;
+//    //Boxes.Add(Box);
+//    //Box_Number++;
+//    //Renderer color = Box.GetComponent<Renderer>();
+//    //color.material.color = new Color(Random.Range(0, 255), Random.Range(0, 255), Random.Range(0, 255));
+//}
