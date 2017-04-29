e4d7098dcf2a0f5588d2dd0d92a28d779ca01ddd
Donray Williams <w.donray@yahoo.com>
2017-04-28 22:11:30 -0500

    Changed win and added reload

--- Assets/Scripts/Encounter.cs
+++ Assets/Scripts/Encounter.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Encounter : MonoBehaviour
 {
@@ -10,10 +11,12 @@ public class Encounter : MonoBehaviour
     private Renderer rend;
     [SerializeField]
     private GameBehavior Goal_Test;
+    private bool Goal_Got;
     private void Start()
     {
         Goal_Test = FindObjectOfType<GameBehavior>();
         rend = GetComponentInParent<Renderer>();
+        Goal_Got = false;
         //Goal_Test.GetComponent<Renderer>().material.color = Color.yellow;
     }
     void OnTriggerEnter(Collider other)
@@ -23,8 +26,21 @@ public class Encounter : MonoBehaviour
             rend.material.color = Random.ColorHSV();
             if (this.gameObject == Goal_Test.Goal)
             {
+                Goal_Got = true;
                 Debug.Log("You win");
             }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        rend.material.color = Color.white;
+    }
+    private void Update()
+    {
+        if (Goal_Got == true)
+        {
+            rend.material.color = Random.ColorHSV();
+            SceneManager.LoadScene("0.main");
         }
     }
 }
